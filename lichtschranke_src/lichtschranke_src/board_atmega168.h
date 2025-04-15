9de7fbc1baa4cf82def1b7dcd8b139cdea63d65d
Tim Sandner <tim-sandner@gmx.de>
2025-04-15 22:25:13 +0200

    Added ATmega168 board pin definitions

---
+++ lichtschranke_src/lichtschranke_src/board_atmega168.h
@@ -0,0 +1,22 @@
+/**
+  * Definition for the Arduino UNO board.
+  * Contains pins
+  */
+
+// interrupt pins used for the light barrier
+const uint8_t PIN_INT_0 = 1;
+const uint8_t PIN_INT_1 = 0;
+
+// display drivers shared pins
+const uint8_t PIN_BCD_A = 8;
+const uint8_t PIN_BCD_B = 9;
+const uint8_t PIN_BCD_C = 10;
+const uint8_t PIN_BCD_D = 11 ;
+const uint8_t PIN_nLT = 15; // light test
+const uint8_t PIN_nBI = 14; // blank
+// individual latch enable pins
+const uint8_t PIN_LE_0 = 16;
+const uint8_t PIN_LE_1 = 17;
+const uint8_t PIN_LE_2 = 18;
+const uint8_t PIN_LE_3 = 19;
+
